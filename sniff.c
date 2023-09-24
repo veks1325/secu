28aece1fdbaade577b7d7578c2df426508c8c80f
veks1325 <53151948+veks1325@users.noreply.github.com>
2023-09-24 22:01:26 +0900

    Update sniff.c

--- sniff.c
+++ sniff.c
@@ -1,3 +1,41 @@
+#include <stdlib.h>
+#include <pcap.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <string.h>
+struct ethheader {
+    u_char  ether_dhost[6]; /* destination host address */
+    u_char  ether_shost[6]; /* source host address */
+    u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
+};
+
+/* IP Header */
+struct ipheader {
+    unsigned char      iph_ihl:4, //IP header length
+    iph_ver:4; //IP version
+    unsigned char      iph_tos; //Type of service
+    unsigned short int iph_len; //IP Packet length (data + header)
+    unsigned short int iph_ident; //Identification
+    unsigned short int iph_flag:3, //Fragmentation flags
+    iph_offset:13; //Flags offset
+    unsigned char      iph_ttl; //Time to Live
+    unsigned char      iph_protocol; //Protocol type
+    unsigned short int iph_chksum; //IP datagram checksum
+    struct  in_addr    iph_sourceip; //Source IP address
+    struct  in_addr    iph_destip;   //Destination IP address
+};
+struct tcpheader {
+    u_short tcp_sport;               /* source port */
+    u_short tcp_dport;               /* destination port */
+    u_int   tcp_seq;                 /* sequence number */
+    u_int   tcp_ack;                 /* acknowledgement number */
+    u_char  tcp_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
+    u_char  tcp_flags;
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
 #define TH_ACK  0x10
 #define TH_URG  0x20
 #define TH_ECE  0x40
