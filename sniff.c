9a1ebe101bc7a0a1496cce4413d57d4d61e2e80a
veks1325 <53151948+veks1325@users.noreply.github.com>
2023-09-24 22:00:01 +0900

    Update sniff.c

--- sniff.c
+++ sniff.c
@@ -8,11 +8,15 @@
     u_short tcp_urp;                 /* urgent pointer */
 };
 
-    void got_packet(u_char *args, const struct pcap_pkthdr *header,
-                const u_char *packet)
+
+#define IP_HL(ip) (((ip)->iph_ihl) & 0x0f)
+ void got_packet(u_char *args, const struct pcap_pkthdr *header,const u_char *packet)
 {
     struct ethheader *eth = (struct ethheader *)packet;
-
+    int payload_len;
+    char *payload;
+    u_int size_ip;
+    u_int size_tcp;
     if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
         struct ipheader *ip = (struct ipheader *)
                 (packet + sizeof(struct ethheader));
@@ -22,15 +26,31 @@
         printf("\n");
         printf(" mac     To:");
         for (int i = 0; i < 6; ++i)
-            printf("%03x:", eth->ether_dhost[i]);
+            printf("%02x:", eth->ether_dhost[i]);
         printf("\n");
         printf(" ip    From: %s\n", inet_ntoa(ip->iph_sourceip));
         printf(" ip      To: %s\n", inet_ntoa(ip->iph_destip));
         struct tcpheader *tcp=(struct tcpheader*)(ip+sizeof(struct ipheader));
         printf(" port  From: %d\n",tcp->tcp_sport);
         printf(" port    To: %d\n",tcp->tcp_dport);
+        payload = (u_char*) (packet + 14);
+        payload_len=ntohs(ip->iph_len)-(size_ip+size_tcp);
+        size_ip = IP_HL(ip)*4;
+        size_tcp = TH_OFF(tcp)*4;
+        if(payload_len==0){
+          return;
+        }
+        for(int i=0;i<payload_len;i++){
+                 printf("%02x", payload[i]);
+                 if(i % 8 == 0) printf("  ");
+                 if(i % 16 == 0) printf("\n");
+
+        }
+        printf("\n");
     }
 }
+
+
 
 
 int main() {
